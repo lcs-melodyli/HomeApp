59045817b05bf9b2113e26bfe511c3d2f31ad437
Melody Li <zli@lcs.on.ca>
2024-01-11 19:08:41 -0500

    Initial Commit

---
+++ HomeApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HomeApp
+//
+//  Created by 利梓萌 on 2024-01-11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HomeApp/HomeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeAppApp.swift
+//  HomeApp
+//
+//  Created by 利梓萌 on 2024-01-11.
+//
+
+import SwiftUI
+
+@main
+struct HomeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
